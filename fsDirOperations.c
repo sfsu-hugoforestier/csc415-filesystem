d9f57b77e5d1b8d6fc300b5859735034609de323
Student <adelaro2@mail.sfsu.edu>
2021-11-09 22:42:20 -0800

    fs_openDir sprint

--- fsDirOperations.c
+++ fsDirOperations.c
@@ -13,12 +13,20 @@
 
 #include "fsDirOperations.h"
 #include "vcb.h"
+#include "fsParsePath.h"
 
-fdDir *fs_openDir(const char *name) {
+fdDir* fs_openDir(const char *name) {
+    //vcb malloc'd by this point\
+    //bogus code, working through compiler faults
     st_vcb *sVCB = returnVCBRef();
     struct st_directory *nDir = parsePath(sVCB->startDirectory, sVCB->blockSize, name);
-
+    int dir = opendir(name);
+    if (dir == NULL){
+        printf("No such directory\n");
+        return NULL;
+    }
     // CHECK IF VALUE RETURNED CORRECT OR NOT
     //fdDir *fDir = malloc();
-
+    printf("--------------Directory Found----------------\n");
+    return 0;
 }
