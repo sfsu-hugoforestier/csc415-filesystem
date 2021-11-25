694122ca28eb89751b1f55780e1614971f55308c
Student <adelaro2@mail.sfsu.edu>
2021-11-24 19:26:24 -0800

    debug and complete dirInfo read

--- fsDirOperations.c
+++ fsDirOperations.c
@@ -7,7 +7,7 @@
 * File: fsDirOperations.h
 *
 * Description:
-* Directory operations
+* Directory operations and neccessary helper functions for directory traversal
 *
 **************************************************************/
 
@@ -16,46 +16,45 @@
 #include "fsParsePath.h"
 #include "mfs.h"
 
-
-struct fs_diriteminfo *fs_readdir(fdDir *dirp) {
-    struct fs_diriteminfo *dirInfo = malloc(sizeof(struct fs_diriteminfo) * dirp->d_reclen);
-
-    if (dirInfo == NULL) {
-        printf("Error while mallocing dirInfo\n");
-        return (NULL);
-    }
-
-    strcpy(dirInfo->d_name, dirp);
-    //strcpy(dirInfo->d_name, );
-
-    // loop for as long as d_reclen is greater than entry position
-    dirInfo->d_reclen = dirp->d_reclen;
-    //finish loop
-    if (dirInfo->fileType = FT_DIRECTORY) {
-    }
-    //dirInfo->fileType = ;
-    return (dirInfo);
-} //while this is no NULL print 
-
 fdDir *fs_openDir(const char *name) {
     st_vcb *sVCB = returnVCBRef();
     fdDir *fDir = malloc(sizeof(fdDir));
+    //will this work with updated parse path?
     struct st_directory *nDir = parsePath(sVCB->startDirectory, sVCB->blockSize, name);
 
     // CHECK IF VALUE RETURNED CORRECT OR NOT
-    parsePath();
-
+    //parsePath();
     if (fDir == NULL) {
         printf("Error while mallocing fDir\n");
         return (NULL);
     }
 
+    //first directory in open at block location with our nDir will be written over
     fDir->directoryStartLocation = nDir->startBlockNb;
     fDir->dirEntryPosition = 0;
     fDir->d_reclen = nDir->nbDir;
+    //fDir->fs_diriteminfo = ;
     printf("--------------Directory Found----------------\n");
     return (fDir);
 }
+
+struct fs_diriteminfo *fs_readdir(fdDir *dirp) {
+    struct fs_diriteminfo *dirInfo = malloc(sizeof(struct fs_diriteminfo) * dirp->d_reclen);
+    //int *dirInfo = *dirp->fs_diriteminfo;
+    if (dirInfo == NULL) {
+        printf("Error while mallocing dirInfo\n");
+        return (NULL);
+    }
+    char path[64];
+    strcpy(dirInfo->d_name, path);
+    //struct st_directory *rDir;
+    while (dirp->directoryStartLocation != NULL){
+        for(int i = dirp->dirEntryPosition; i < dirp->d_reclen){
+            parsePath()
+        }
+    }
+    return (dirInfo);
+}  
 
 int fs_closedir(fdDir *dirp) {
     return 0;
