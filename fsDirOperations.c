a5b689f56579c8d62e7f2c9ea3d9666fdbf87997
centroid7@gmail.com <adelaro2@mail.sfsu.edu>
2021-11-29 12:50:39 -0800

    Test Stat buf and opendir calls in shell lines 200, 220

--- fsDirOperations.c
+++ fsDirOperations.c
@@ -8,7 +8,7 @@
 *
 * Description:
 * Directory operations and neccessary helper functions for directory traversal
-*
+* Wanted to make linker functions to the shell. Undefined reference errors in fs 
 **************************************************************/
 
 #include "fsDirOperations.h"
@@ -16,26 +16,34 @@
 #include "fsParsePath.h"
 #include "mfs.h"
 
+struct fs_diriteminfo *dirInfo;
+
 fdDir *fs_openDir(const char *name) {
     st_vcb *sVCB = returnVCBRef();
     fdDir *fDir = malloc(sizeof(fdDir));
+    struct st_directory * incomingDir;
     //  TODO: ERROR implicit declaration of function ‘parsePath’
     //  TODO: ERROR initialization makes pointer from integer without a cast
-    struct st_directory * nDir = parsePath(sVCB->startDirectory, sVCB->blockSize, name);
-
     // CHECK IF VALUE RETURNED CORRECT OR NOT
     //parsePath();
-    if (fDir == NULL) {
-        printf("Error while mallocing fDir\n");
+    if (fDir == NULL){
+        printf("Error mallocing fDir");
+        return NULL;
+    }
+
+    incomingDir = parsePath(sVCB->startDirectory, sVCB->blockSize, name);
+    if (incomingDir == NULL) {
+        printf("Error locating directory");
         return (NULL);
     }
 
     //first directory in open at block location with our nDir will be written over
-    fDir->directoryStartLocation = nDir->startBlockNb;
-    fDir->dirEntryPosition = 0;
-    fDir->d_reclen = nDir->nbDir;
     printf("--------------Directory Found----------------\n");
-    return (fDir);
+    fDir->directoryStartLocation = incomingDir->startBlockNb;
+    fDir->dirEntryPosition = incomingDir->nbDir;
+    fDir->d_reclen = incomingDir->sizeDirectory;
+    fDir->fs_dirItemInfoPointer = &dirInfo;
+    return(&fDir);
 }
 
 struct fs_diriteminfo *fs_readdir(fdDir *dirp) {
@@ -49,13 +57,14 @@ struct fs_diriteminfo *fs_readdir(fdDir *dirp) {
     }
     // TODO: assignment makes integer from pointer without a cast 
     if (dirp->directoryStartLocation = NULL){
+        //reset to logical block address of '.'
         dirp->directoryStartLocation = 0;
         return (NULL);
     }
     //nbDir = dirEntry[0]
     if(index < dirp->d_reclen)
     {
-        dirp->dirEntryPosition = dirp->dirEntryPosition + 1; 
+        dirp->directoryStartLocation = dirp->dirEntryPosition; 
         char path[64];
         strcpy(dirInfo->d_name, path);
         dirp->d_reclen = dirInfo->d_reclen;
@@ -71,16 +80,25 @@ int fs_closedir(fdDir *dirp) {
 
 int fs_stat(const char *path, struct fs_stat *buf) {
     st_vcb *sVCB = returnVCBRef();
+    struct st_directory *fDir;
     const char *name = path;
     // TODO: initialization makes pointer from integer without a cast
-    struct st_directory *fDir = parsePath(sVCB->startDirectory, 512, path);
+    fDir = parsePath(sVCB->startDirectory, 512, path);
+    if (fDir == NULL){
+        printf("Directory stats not retrievable from incoming path");
+        return 0;
+    }
+    //read from our 
+    LBAread(buf,sVCB->numberOfBlocks,sVCB->startDirectory);
     if(name){
         //size in bytes from malloc of directory
         buf->st_size = fDir->sizeDirectory;
         // this will probably nag about casting
-        buf->st_blksize = (fDir->sizeDirectory / 512);
+        buf->st_blksize = (fDir->sizeDirectory + 1 / 512);
         buf->st_createtime = fDir->creationDate;
         buf->st_modtime = fDir->lastModDate; 
     }
     //pull back Hugo's parsepath and set our stats with buffer
+    //fix the reference error in lines 200 of the shell, argvc[k]. arguments coming in ./* for all
+    // two arguments or more
 }
--- mfs.h
+++ mfs.h
@@ -52,7 +52,7 @@ typedef struct
 	unsigned short  d_reclen;		/*length of this record */
 	unsigned short	dirEntryPosition;	/*which directory entry position, like file pos */
 	uint64_t	directoryStartLocation;		/*Starting LBA of directory */
-	//int *fs_diriteminfo; /* pointer to our item info */
+	int * fs_dirItemInfoPointer; /* pointer to our item info */
 	} fdDir;
 
 // Key directory functions
